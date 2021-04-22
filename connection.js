55f89466f46b99ee86d5da730c37bdaffff6987c
phellutone <72784756+phellutone@users.noreply.github.com>
2021-04-23 03:35:11 +0900

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,32 @@ let HOST = "wss://mso5-ws-s.herokuapp.com/" //ws server address
 let ws = new WebSocket(HOST);
 let el;
 
+let bstate = false
+let button = document.getElementById("button");
+button.addEventListener("click", e => {
+  e.preventDefault();
+  
+  if(DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === "function"){
+    DeviceMotionEvent.requestPermission();
+  }
+  
+  if(bstate){
+    window.removeEventListener("devicemotion");
+    document.getElementById("button").innerHTML = "start";
+    bstate = false
+  }else{
+    window.addEventListener("devicemotion", e => {
+      ws.send(e.acceleration.x+":"+e.acceleration.y+":"+e.acceleration.z);
+    });
+    document.getElementById("button").innerHTML = "stop";
+    bstate = true;
+  }
+});
+
+
+
 ws.onmessage = (event) => {
   el = document.getElementById('ws');
   el.innerHTML = event.data;
 };
+
